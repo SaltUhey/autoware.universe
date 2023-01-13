77e3c79e46c3f1ba6cafe4871d86b8c2291aa96b
yuhei <yuhei1030.mini.normal@gmail.com>
2023-01-13 12:01:32 +0900

    <weighted down sampling>

--- sensing/tier4_pcl_extensions/include/tier4_pcl_extensions/oshikubo_use.hpp
+++ sensing/tier4_pcl_extensions/include/tier4_pcl_extensions/oshikubo_use.hpp
@@ -9,26 +9,6 @@
 #include <boost/random/variate_generator.hpp> // for variate_generator
 #include <random>
 #include <fstream>
-
-
-
-
-int size_d1,size_d2,size_d3;
-//std::vector<int> duplication_check_d1,duplication_check_d2,duplication_check_d3;
-std::vector<int> vec_num_output_d1,vec_num_output_d2,vec_num_output_d3;
-
-//ある範囲内での整数を一様分布で発生させる関数の定義
-        //std::random_device rd; 
-        //static std::mt19937 gen(std::random_device rd()); //Mersenne twister algorithm
-        //int low,high;
-        //std::uniform_int_distribution<> ud_int(int low, int high);
-        
-        //std::random_device seed_gen;
-        //static std::default_random_engine engine(std::random_device seed_gen());
-        //float f_low, f_high;
-        //std::uniform_real_distribution<float> ud(float f_low, float f_high);
-
-        //std::vector<int> duplication_check;
         
 int random_int(int low,int high);//{return int_random;}
 float random_float(float f_low,float f_high);//{return float_random;}
